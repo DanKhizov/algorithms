255c090fcce5710c977afe8a1c724ed1c8521d1a
Daniiil Khizov <dan.khizov@gmail.com>
2018-10-25 12:18:51 +0300

    refactore code

--- Sort algorithms /bubbleSortDown.js
+++ Sort algorithms /bubbleSortDown.js
@@ -1,13 +1,10 @@
-let arr = [12, 2, 45, 6, 32, 357, 24, 6346, 123, 75],
-    newArr = [];
+let arr = [12, 2, 45, 6, 32, 357, 24, 6346, 123, 75];
 
 function bubbleSort(arr) {
-    var length = arr.length;
-
-    for (var i = 0; i < length; i++) {
-        for (var j = 0; j < length - i - 1; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                var tmp = arr[j];
+                let tmp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = tmp;
             }
--- Sort algorithms /bubbleSortUp.js
+++ Sort algorithms /bubbleSortUp.js
@@ -1,17 +1,16 @@
-let arr = [12, 2, 45, 6, 32, 357, 24, 6346, 123, 75],
-    newArr = [];
+let arr = [12, 2, 45, 6, 32, 357, 24, 6346, 123, 75];
 
-var bubbleSort = function(arr) {
+function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - 1; j++) {
             if (arr[j + 1] > arr[j]) {
-                var temp = arr[j];
+                let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
     return arr;
-};
+}
 
 console.log(bubbleSort(arr));
