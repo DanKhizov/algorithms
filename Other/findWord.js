fcd4714f44d0ee8b9934244037685cfbff1e8781
Daniiil Khizov <dan.khizov@gmail.com>
2018-10-28 03:37:44 +0300

    new alghorithm

--- Other/findWord.js
+++ Other/findWord.js
@@ -1,14 +1,14 @@
-var paragraph = "Bob hit a ball, the hit BALL flew far after it was hit";
+let paragraph = "Bob hit a ball, the hit BALL flew far after it was hit";
 
 function fillArray(paragraph) {
-    var newWord = "";
-    var regexp = /,/;
-    var words = [];
+    let newWord = "";
+    let regexp = /,/;
+    let words = [];
 
     paragraph = paragraph.replace(regexp, "").toLowerCase();
     paragraph += " ";
 
-    for (var i = 0; i < paragraph.length; i++) {
+    for (let i = 0; i < paragraph.length; i++) {
         if (paragraph[i] === " ") {
             words.push(newWord);
             newWord = "";
@@ -16,7 +16,25 @@ function fillArray(paragraph) {
             newWord += paragraph[i];
         }
     }
-    console.log(words);
+
+    let freq = {},
+        max = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        if (freq[word]) {
+            freq[word]++;
+        } else {
+            freq[word] = 1;
+        }
+
+        if (freq[word] > max) {
+            max = freq[word];
+            result = word;
+        }
+    }
+
+    return result;
 }
 
-fillArray(paragraph);
+console.log(fillArray(paragraph)); // hit
