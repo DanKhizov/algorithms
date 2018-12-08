8605111b736f1fcf2c2b9cbe1904dcb43a0c736a
Даниил Хизов <43781799+DanKhizov@users.noreply.github.com>
2018-12-09 01:48:29 +0300

    Update BitAdder

--- Other/bitAdder.js
+++ Other/bitAdder.js
@@ -1,50 +1,24 @@
-Number.prototype.binary = function() {
-	let bin = +this.toString(2);
-	return bin.toString();
-};
+function adder(num1, num2) {
+  for (let i = 0; num1[i] || num2[i]; i++) {
+    if (!num1[i]) num1 = "0" + num1;
+    if (!num2[i]) num2 = "0" + num2;
+  }
 
-String.prototype.reverse = function() {
-	return this.split('').reverse().join('');
-};
+  let p = 0;
+  let s = [];
 
-function toDecimal(num) {
-	let dec = Number(num);
-	return parseInt(dec, 2).toString();
+  for (let i = num2.length - 1; i >= 0; i--) {
+    let a = num1[i];
+    let b = num2[i];
+
+    s[i + 1] = p ^ (b ^ a);
+    p = (p & (a ^ b)) | (a & b);
+
+    if (i === 0) s[i] = p;
+  }
+
+  return console.log(s.join(""), "=", parseInt(Number(s.join("")), 2));
 }
 
-const binString = (x, y) => {
-	let a = x.binary();
-	let b = y.binary();
-
-	let size = x > y ? a.length : b.length;
-
-	for (let i = 0; i < size; i++) {
-		if (!a[i]) {
-			a = '0' + a;
-		}
-		if (!b[i]) {
-			b = '0' + b;
-		}
-	}
-
-	return { a, b };
-};
-
-let sumNums = binString(5205, 42);
-let = 0;
-let s = '';
-
-for (let i = sumNums.a.length - 1; i >= 0; i--) {
-	let a = sumNums.a[i];
-	let	b = sumNums.b[i];
-
-	s += p ^ (b ^ a);
-	p = (p & (a ^ b)) | (a & b);
-
-	if (i === 0) {
-		s += p;
-		s = s.reverse();
-	}
-}
-
-console.log(s, '=', toDecimal(s));
+adder("1010001010101", "101010");
+adder("101010", "1010001010101");
