f32d7967e735794d5336af7de0f297f84a110167
Daniiil Khizov <dan.khizov@gmail.com>
2018-12-21 05:19:20 +0300

    new algorithms

---
+++ reverseInt.js
@@ -0,0 +1,15 @@
+const int = -123;
+
+function reverseInt(int) {
+  strReverse = String(int)
+    .split("")
+    .reverse()
+    .map(char => (char === "-" ? "" : char))
+    .join("");
+
+  console.log(strReverse);
+
+  return Number(strReverse) * Math.sign(int);
+}
+
+console.log(reverseInt(int));
